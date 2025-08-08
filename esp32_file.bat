2afe11d0601f257cf5cd70384ccaca35964b5077
enesvardar <enesvrdr0@gmail.com>
2025-08-09 00:20:14 +0300

    Auto commit Cmt 09.08.2025  0:20:14,81

---
+++ esp32_file.bat
@@ -0,0 +1,26 @@
+@echo off
+echo === Copying BIN file ===
+
+set "SOURCE_BIN=D:\github\solar-tracking-system-esp32-communication\.pio\build\esp32-s3-devkitc-1\firmware.bin"
+set "DEST_FOLDER=D:\github\firmware"
+copy /Y "%SOURCE_BIN%" "%DEST_FOLDER%"
+echo BIN file copied.
+
+echo.
+echo === Retrieving and writing version information ===
+
+set "DEFINES_FILE=D:\github\solar-tracking-system-esp32-communication\src\main.cpp"
+set "VERSION_FILE=%DEST_FOLDER%\esp32_version_info.txt"
+
+REM Search for #define APP_VERSION line and get the 3rd token
+for /f "tokens=3 delims= " %%A in ('findstr /C:"#define APP_VERSION" "%DEFINES_FILE%"') do (
+    set "VERSION=%%~A"
+)
+
+REM Use call to expand the variable and write to file
+call echo APP_VERSION %VERSION:"=%> "%VERSION_FILE%"
+
+echo Version information written: APP_VERSION %VERSION:"=% 
+echo.
+echo All operations completed.
+exit
