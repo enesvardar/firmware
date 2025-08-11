b763bbf7bedaf0e4c5465a05015d98da418ce20f
enesvardar <enesvrdr0@gmail.com>
2025-08-11 14:56:27 +0300

    Auto commit Pzt 11.08.2025 14:56:27,73

--- github.bat
+++ github.bat
@@ -11,7 +11,7 @@ echo Committing...
 git commit -m "Auto commit %date% %time%" || echo "No changes to commit."
 
 echo Pushing changes...
-git push --force origin main
+git push origin main
 
 echo Process completed!
 exit
