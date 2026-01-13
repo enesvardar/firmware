bfb99d34498b397dfeb52ae99d60d90ada5f4d3a
enesvardar <enesvrdr0@gmail.com>
2026-01-13 13:52:06 +0300

    Auto commit Sal 13.01.2026 13:52:06,95

--- stm32_file.bat
+++ stm32_file.bat
@@ -1,7 +1,7 @@
 @echo off
 echo === Copying HEX file ===
 
-set SOURCE_HEX=D:\github\solar-tracking-system-internal-acc\Objects\solar_tracking_system_internal_acc.hex
+set SOURCE_HEX=D:\github\internal\solar-tracking-system-internal-acc\Objects\solar_tracking_system_internal_acc.hex
 set DEST_FOLDER=D:\github\firmware
 copy /Y "%SOURCE_HEX%" "%DEST_FOLDER%"
 echo HEX file copied.
@@ -9,7 +9,7 @@ echo HEX file copied.
 echo.
 echo === Retrieving and formatting version information ===
 
-set DEFINES_FILE=D:\github\solar-tracking-system-internal-acc\src\defines.h
+set DEFINES_FILE=D:\github\internal\solar-tracking-system-internal-acc\src\defines.h
 set VERSION_FILE=%DEST_FOLDER%\stm32_version_info.txt
 
 REM Search for #define APP_VERSION line and get the 3rd token
